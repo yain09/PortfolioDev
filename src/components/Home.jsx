51a11e26f3f01cea2ec88b9bdd251f45c12c8997
yain09 <yain09@gmail.com>
2024-06-13 01:18:51 -0300

    ajustes botones - Release 1.0

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,36 +21,84 @@ function Home() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
   return (
     <>
       {windowWidth < 576 && (
-        <>
+        <div
+          className="m-0 row align-items-center d-flex"
+          style={{
+            padding: "16px",
+            position: "relative",
+            zIndex: 1,
+            backgroundImage: `url(${bg})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            minHeight: "300px",
+          }}
+        >
           <div
-            className="m-0 row align-items-center d-flex"
+            xs={10}
+            className="d-flex yain"
             style={{
-              padding: "16px",
-              position: "relative",
+              width: "613px",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              background: "rgba(245, 245, 245, 0.9)",
+            }}
+          >
+            <div className="text-center">
+              <p className="pixel2 mb-1">Yain Godoy</p>
+              <p className="silka2">
+                Desarrollador Frontend <br /> con interés en UI/UX
+              </p>
+              <div className="icon">
+                <a
+                  href="https://www.linkedin.com/in/yain-andres-godoy/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+                <a
+                  href="https://github.com/yain09"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+                <a
+                  href="https://drive.google.com/file/d/1mqqPbyRAKe6YSbgztZ2ZPfK2HAp_rlwU/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+              </div>
+            </div>
+            <img src={yo} className="yo" alt="Yain Godoy" />
+          </div>
+        </div>
+      )}
+
+      {windowWidth > 576 && (
+        <div className="m-0 px-3">
+          <div
+            className="banner m-0 p-0 row align-items-center d-flex"
+            style={{
               zIndex: 1,
               backgroundImage: `url(${bg})`,
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
-              minHeight: "300px",
             }}
           >
-            <div
-              xs={10}
+            <Col className="col-xs-auto" />
+            <Col
               className="d-flex yain"
               style={{
-                Width: "613px",
+                width: "613px",
                 justifyContent: "space-evenly",
                 alignItems: "center",
-                background: "rgba(245, 245, 245, 0.9)"
               }}
             >
-              <div className="text-center">
+              <div>
                 <p className="pixel2 mb-1">Yain Godoy</p>
-                <p className="silka2 ">
-                  Desarrollador Frontend <br /> con interes en UI/UX
+                <p className="silka2">
+                  Desarrollador Frontend <br /> con interés en UI/UX
                 </p>
                 <div className="icon">
                   <a
@@ -70,62 +118,11 @@ function Home() {
                   />
                 </div>
               </div>
-              <img src={yo} className="yo" alt="" />
-            </div>
+              <img src={yo} className="yo" alt="Yain Godoy" />
+            </Col>
+            <Col className="col-xs-auto" />
           </div>
-        </>
-      )}
-      {windowWidth > 576 && (
-        <>
-          <div className="m-0 px-3">
-            <div
-              className="banner m-0 p-0 row align-items-center d-flex"
-              style={{
-                position: "relative",
-                zIndex: 1,
-                backgroundImage: `url(${bg})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              <Col className="col-xs-auto" />
-              <Col
-                className="d-flex yain"
-                style={{
-                  Width: "613px",
-                  justifyContent: "space-evenly",
-                  alignItems: "center",
-                }}
-              >
-                <div className="">
-                  <p className="pixel2 mb-1">Yain Godoy</p>
-                  <p className="silka2 ">
-                    Desarrollador Frontend <br /> con interes en UI/UX
-                  </p>
-                  <div className="icon">
-                    <a
-                      href="https://www.linkedin.com/in/yain-andres-godoy/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    />
-                    <a
-                      href="https://github.com/yain09"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    />
-                    <a
-                      href="https://drive.google.com/file/d/1mqqPbyRAKe6YSbgztZ2ZPfK2HAp_rlwU/view?usp=sharing"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    />
-                  </div>
-                </div>
-                <img src={yo} className="yo" alt="" />
-              </Col>
-              <Col className="col-xs-auto" />
-            </div>
-          </div>
-        </>
+        </div>
       )}
 
       <div
@@ -134,7 +131,7 @@ function Home() {
       >
         <Cards2
           title="SEI"
-          txt='Pagina web de la asignatura "Sistemas Estructurales I" de la Facultad de Arquitectura de la UNL.'
+          txt='Página web de la asignatura "Sistemas Estructurales I" de la Facultad de Arquitectura de la UNL.'
           img={study}
           link="/PortfolioDev-SEI"
         />
@@ -146,7 +143,7 @@ function Home() {
         />
         <Cards2
           title="Portfolio ARQ"
-          txt="Portfolio profesional de Arquitectura donde se condensan mis trabajos desarrollados en los ultimos años"
+          txt="Portfolio profesional de Arquitectura donde se condensan mis trabajos desarrollados en los últimos años."
           img={work}
           link="/PortfolioDev-PortfolioArq"
         />
@@ -154,4 +151,5 @@ function Home() {
     </>
   );
 }
+
 export default Home;
